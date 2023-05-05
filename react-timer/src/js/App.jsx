198a24dc1bce23be6eff60db382de0e9d1abbab7
Alexander Babel <babel.softwaresolutions-freelance@digitalcareerinstitute.org>
2023-05-05 12:17:26 +0200

    Added comments for timer component and added some conditional rendering part in the App component of react-timer

--- react-timer/src/js/App.jsx
+++ react-timer/src/js/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 import '../css/App.css';
 import Timer from './components/Timer';
 
 function App() {
+  const [timerVisible, setTimerVisible] = useState(false);
+
+  function showTimerChangeHandler(evt) {
+    const isChecked = evt.target.checked;
+    setTimerVisible(isChecked);
+  }
+
+  // conditional rendering outside the JSX (keeps the JSX cleaner)
+  const timerPart = timerVisible ? <Timer time={10} /> : <p>No Timer today</p>;
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <Timer />
+
+      <label htmlFor="show-timer-chkbox">Show Timer</label>
+      <input type="checkbox" id="show-timer-chkbox" onChange={showTimerChangeHandler} />
+      
+      {
+        // Conditional rendering inside of JSX
+
+        // Show/Hide depending on a condition
+        //timerVisible && <Timer time={10} />
+
+        // Show one thing or another depending on condition
+        timerVisible ? <Timer time={10} /> : <p>No Timer today</p>
+      }
+
+      {timerPart}
 
     </div>
   );
--- react-timer/src/js/components/Timer.jsx
+++ react-timer/src/js/components/Timer.jsx
@@ -1,43 +1,63 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-export default function Timer() {
+export default function Timer({time}) {
     // timer set to 10 seconds initally
-    const [timer, setTimer] = useState(10);
+    const [timer, setTimer] = useState(time);
     const [isRunning, setIsRunning] = useState(false);
 
+    // create a side-effect depending on changes of the isRunning state variable
     useEffect(() => {
+        // Variable to store the interval reference
         let interval;
+
+        // if running state has been set to true
         if (isRunning) {
             console.log('The timer is running!');
+
+            // Start a new interval, which is going to run once a second (1000ms)
             interval = setInterval(() => {
-                if (timer !== 0) {
+                // if the timer state variable is still greater than 0
+                if (timer > 0) {
+                    // decrease it by one
                     setTimer(curr => curr-1);
                 }
             }, 1000);
 
-        } else {
+        } else { // isRunning has been set to false
             console.log('The timer stopped!');
+
+            // clear the interval, it's not gonna run anymore
             clearInterval(interval);
         }
 
+        // return a clean-up function which clears the running interval to make sure
+        // that there's always just one of them running at once
         return () => clearInterval(interval);
     }, [isRunning]);
 
+    // a side-effect for the timer change
     useEffect(() => {
+        // if the timer state variable reaches 0, it stops the timer
         if (timer < 1) setIsRunning(false);
     }, [timer]);
 
+    // click handler for the start button
     function startHandler(evt) {
+        // start the timer
         setIsRunning(true);
     }
 
+    // click handler for the stop button
     function stopHandler(evt) {
+        // stop the timer
         setIsRunning(false);
     }
 
+    // click handler for the reset button
     function resetHandler(evt) {
-        setTimer(10);
+        // set the timer (state variable) to its inital value
+        setTimer(time);
     }
 
     return (
